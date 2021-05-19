cca4b4dc1e350b53a354c381f290e4379990bed9
Andreas Tsika <andreastsika@gmail.com>
2021-05-19 18:36:02 +0300

    style changes on active route in sidebar links

--- resources/views/components/layouts/sidebar.blade.php
+++ resources/views/components/layouts/sidebar.blade.php
@@ -16,7 +16,8 @@
         <nav class="mt-10">
             <ul class="text-gray-400">
                 <li>
-                    <a class="flex items-center mt-4 py-2 px-4 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
+                    <a class="flex items-center mt-4 py-2 px-4 hover:bg-gray-700 hover:bg-opacity-50 hover:text-gray-100
+                        {{request()->is('dashboard')? 'bg-gray-600 text-white' : ''}}"
                        href="{{route('dashboard')}}">
                         <span>
                             <i class="fas fa-tachometer-alt"></i>
@@ -25,18 +26,20 @@
                     </a>
                 </li>
                 <li x-data="{selected:null}">
-                    <div class="flex items-center mt-4 py-2 px-4 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100">
+                    <div class="flex items-center mt-4 py-2 px-4 hover:bg-gray-700 hover:bg-opacity-50 hover:text-gray-100">
                         <span><i class="fas fa-file-alt"></i></span>
                         <span class="mx-4 cursor-pointer"
                               @click="selected !== 0 ? selected = 0 : selected = null">{{__("app.protocol")}}</span>
                     </div>
                     <div x-show="selected == 0" class="bg-gray-800">
-                        <a class="flex items-center mt-4 py-2 px-10 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
+                        <a class="flex items-center mt-4 py-2 px-10 hover:bg-gray-700 hover:bg-opacity-50 hover:text-gray-100
+                            {{request()->is('protocol/create/ingoing')? 'bg-gray-600 text-white' : ''}}"
                            href="{{route('protocol.create',['type' => 'ingoing'])}}">
                             <span><i class="fas fa-file-import"></i></span>
                             <span class="mx-3">{{__('app.ingoing')}}</span>
                         </a>
-                        <a class="flex items-center mt-4 py-2 px-10 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
+                        <a class="flex items-center mt-4 py-2 px-10 hover:bg-gray-700 hover:bg-opacity-50 hover:text-gray-100
+                            {{request()->is('protocol/create/outgoing')? 'bg-gray-600 text-white' : ''}}"
                            href="{{route('protocol.create',['type' => 'outgoing'])}}">
                             <span><i class="fas fa-file-export"></i></span>
                             <span class="mx-3">{{__('app.outgoing')}}</span>
@@ -44,7 +47,8 @@
                     </div>
                 </li>
                 <li>
-                    <a class="flex items-center mt-4 py-2 px-4 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
+                    <a class="flex items-center mt-4 py-2 px-4 hover:bg-gray-700 hover:bg-opacity-50 hover:text-gray-100
+                        {{request()->is('records')? 'bg-gray-600 text-white' : ''}}"
                        href="{{route('records.index')}}">
                         <span><i class="fas fa-th-list"></i></span>
                         <span class="mx-3">{{__('app.records')}}</span>
@@ -54,7 +58,7 @@
                     <form method="POST" action="{{ route('logout') }}">
                         @csrf
                         <a href="{{route('logout')}}"
-                           class="flex items-center mt-4 py-2 px-4 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
+                           class="flex items-center mt-4 py-2 px-4 hover:bg-gray-700 hover:bg-opacity-50 hover:text-gray-100"
                            onclick="event.preventDefault(); this.closest('form').submit();">
                             <span><i class="fas fa-power-off"></i></span>
                             <span class="mx-3">{{__('app.logout')}}</span>
