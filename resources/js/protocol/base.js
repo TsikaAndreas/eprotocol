917bf98060ce257a536a5beb58f212e7ba63498d
Andreas Tsika <andreastsika@gmail.com>
2021-10-15 02:45:45 +0300

    updated git ignore

---
+++ resources/js/protocol/base.js
@@ -0,0 +1,77 @@
+$(document).ready(function() {
+    $('#content').find("form").submit(function () {
+        $('#form-loader').removeClass('hidden');
+    });
+
+    let protocolReactivateBtn = document.getElementById('reactivateProtocol');
+    let protocolCancelBtn = document.getElementById('cancelProtocol');
+
+    if (protocolReactivateBtn !== null) {
+        protocolReactivateBtn.addEventListener('click',reactivateProtocol);
+    }
+    if (protocolCancelBtn !== null) {
+        protocolCancelBtn.addEventListener('click',cancelProtocol);
+    }
+});
+
+function cancelProtocol() {
+    let protocol = {
+        id: document.getElementById('cancelProtocol').value,
+        name: document.getElementById('protocol').textContent,
+        action: 'cancel'
+    }
+    protocolStatusChange('Cancellation', protocol);
+}
+
+function reactivateProtocol() {
+    let protocol = {
+        id: document.getElementById('reactivateProtocol').value,
+        name: document.getElementById('protocol').textContent,
+        action: 'reactivate'
+    }
+    protocolStatusChange('Reactivation', protocol);
+}
+
+function protocolStatusChange(action, protocol) {
+    let CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
+    Swal.fire({
+        html: '<p>Please confirm the <b>'+ action +'</b> of the following protocol.</p><p><b>' + protocol.name + '</b></p>',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#10b981',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Confirm',
+        showLoaderOnConfirm: true,
+    }).then(function (e) {
+        if (e.isConfirmed) {
+            $.ajax({
+                url: window.location.origin + '/protocol/change-status',
+                method: 'POST',
+                dataType: 'JSON',
+                data: {
+                    _token: CSRF_TOKEN,
+                    id: protocol.id,
+                    action: protocol.action
+                },
+                success: function (results) {
+                    if (results.success) {
+                        Swal.fire('', results.message, "success").then(function (target) {
+                            if (target.isConfirmed || target.isDismissed) {
+                                window.location.reload();
+                            }
+                        });
+                    } else {
+                        Swal.fire('', results.message, "warning").then(function (target) {
+                            if (target.isConfirmed || target.isDismissed) {
+                                window.location.reload();
+                            }
+                        });
+                    }
+                },
+                fail: function (results) {
+                    Swal.fire('', results.message, "error");
+                }
+            });
+        }
+    });
+}
