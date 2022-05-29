e55a2b9eba917f6fbf02e4a78d6bfbb33c9c3e45
Andreas Tsika <andreastsika@gmail.com>
2022-05-29 20:48:51 +0300

    changes

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,13 +6,8 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}"></script>
     </head>
